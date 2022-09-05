94117ea51f20282d8c8d7c35f84f8be6a48c9828
Alok Singh <alokdhari@gmail.com>
2022-09-05 15:11:26 +0100

    Second test done

--- DiamondKata/DiamondPrinter.cs
+++ DiamondKata/DiamondPrinter.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
 public class DiamondPrinter
 {
     public string Print(char midPointCharacter)
@@ -9,6 +11,23 @@ public class DiamondPrinter
             return "A";
         }
 
-        throw new NotImplementedException();
+        var diamond = new StringBuilder();
+        var depth = characterIndex * 2 - 1;
+        var isPrintingBottomHalfOfDiamond = false;
+        for (var index = 0; index < depth; index++)
+        {
+            var charToEnter = (char)(isPrintingBottomHalfOfDiamond 
+                ? 'A' + depth - index - 1 
+                : 'A' + index);
+            diamond.Append(charToEnter);
+            if(charToEnter != 'A')
+            {
+                diamond.Append(charToEnter);
+            }
+
+            isPrintingBottomHalfOfDiamond = isPrintingBottomHalfOfDiamond || 'A' + index == midPointCharacter;
+        }
+
+        return diamond.ToString();
     }
 }
